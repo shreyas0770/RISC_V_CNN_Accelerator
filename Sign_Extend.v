aa0307a6b077271f43a9b4bae4a92077bbf19c9c
shreyas0770 <shreyassridhar2002@gmail.com>
2024-03-03 11:27:25 +0530

    Convolution actually working

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -17,7 +17,7 @@ module Sign_Extend (In,ImmSrc,Imm_Ext);
     input [1:0] ImmSrc;
     output [31:0] Imm_Ext;
 
-    assign Imm_Ext =  (ImmSrc == 2'b00) ? {{20{In[31]}},In[31:20]} : 
+    assign Imm_Ext =  (ImmSrc == 2'b00) ? {{12{In[31]}},In[31:12]} : 
                      (ImmSrc == 2'b01) ? {{20{In[31]}},In[31:25],In[11:7]} : 32'h00000000; 
 
 endmodule
