25fd1a5667971fe5cacc9c72a92c1f322071beca
shreyas0770 <shreyassridhar2002@gmail.com>
2024-04-23 22:59:39 +0530

    trying to add winograd 2d

--- ALU.v
+++ ALU.v
@@ -12,6 +12,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 `include "convolution.v"
+`include "winograd.v"
 module ALU(
     A, B,
     Result,
@@ -31,6 +32,43 @@ module ALU(
     wire [31:0] Sum;
     wire [31:0]conv_output;
 
+    reg [31:0] w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13;
+    wire [31:0] wino_out;
+
+    always @(B)
+    begin
+    if(!w0)
+        w0 = B;
+    else if(!w1)
+        w1 = B;
+    else if(!w2)
+        w2 = B;
+    else if(!w3)
+        w3 = B; 
+    else if(!w4)
+        w4 = B;
+    else if(!w5)
+        w5 = B;    
+    else if(!w6)
+        w6 = B;
+    else if(!w7)
+        w7 = B;
+    else if(!w8)
+        w8 = B;
+    else if(!w9)
+        w9 = B; 
+    else if(!w10)
+        w10 = B;
+    else if(!w11)
+        w11 = B;    
+    else if(!w12)
+        w12 = B;
+    else if(!w13)
+        w13 = B;
+    end
+
+    winograd wino(w0[15:8],w0[7:0],w1[15:8],w1[7:0],w2[15:8],w2[7:0],w3[15:8],w3[7:0],w4[15:8], w4[7:0], w5[15:8],w5[7:0], w6[15:8],w6[7:0],w7[15:8],w7[7:0],w8[15:8],w8[7:0],w9[15:8],w10[15:8],w10[7:0],w11[15:8],w12[15:8],w12[7:0],w13[15:8],wino_out[31:24],wino_out[23:16],wino_out[15:8],wino_out[7:0]);
+
     convolution c1(.A(A),.B(B),.Result(conv_output));
 
     assign Sum = (ALUControl[0] == 1'b0) ? A + B :
@@ -40,7 +78,8 @@ module ALU(
                             (ALUControl == 3'b010) ? A & B :
                             (ALUControl == 3'b011) ? A | B :
                             (ALUControl == 3'b101) ? {{32{1'b0}}, (Sum[31])} :
-                            (ALUControl == 3'b111) ? {1'b0,conv_output}:
+                            //(ALUControl == 3'b111) ? {1'b0,conv_output}:
+                            (ALUControl == 3'b111) ? {wino_out}:
                             (ALUControl == 3'b100) ? {1'b0, B}:
                             {33{1'b0}};
     assign OverFlow = ((Sum[31] ^ A[31]) & 
---
+++ winograd_tb.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+
+module testbench;
+
+    // Inputs
+    reg [7:0] inp10, inp11, inp12, inp13;
+    reg [7:0] inp20, inp21, inp22, inp23;
+    reg [7:0] inp30, inp31, inp32, inp33;
+    reg [7:0] inp40, inp41, inp42, inp43;
+    reg [7:0] ker10, ker11, ker12;
+    reg [7:0] ker20, ker21, ker22;
+    reg [7:0] ker30, ker31, ker32;
+
+    // Outputs
+    wire [7:0] out10, out11, out20, out21;
+
+    // Instantiate the module under test
+    winograd dut(
+        .inp10(inp10), .inp11(inp11), .inp12(inp12), .inp13(inp13),
+        .inp20(inp20), .inp21(inp21), .inp22(inp22), .inp23(inp23),
+        .inp30(inp30), .inp31(inp31), .inp32(inp32), .inp33(inp33),
+        .inp40(inp40), .inp41(inp41), .inp42(inp42), .inp43(inp43),
+        .ker10(ker10), .ker11(ker11), .ker12(ker12),
+        .ker20(ker20), .ker21(ker21), .ker22(ker22),
+        .ker30(ker30), .ker31(ker31), .ker32(ker32),
+        .out10(out10), .out11(out11), .out20(out20), .out21(out21)
+    );
+
+    // Stimulus
+    initial begin
+        // Set inputs
+        inp10 = 8'h1; inp11 = 8'h0; inp12 = 8'h1; inp13 = 8'h0;
+        inp20 = 8'h2; inp21 = 8'h3; inp22 = 8'h1; inp23 = 8'h0;
+        inp30 = 8'h1; inp31 = 8'h2; inp32 = 8'h2; inp33 = 8'h1;
+        inp40 = 8'h2; inp41 = 8'h0; inp42 = 8'h1; inp43 = 8'h0;
+        ker10 = 8'h1; ker11 = 8'h0; ker12 = 8'h1;
+        ker20 = 8'h0; ker21 = 8'h1; ker22 = 8'h0;
+        ker30 = 8'h1; ker31 = 8'h1; ker32 = 8'h1;
+        
+//        inp10 = 8'h0; inp11 = 8'h1; inp12 = 8'h0; inp13 = 8'h1;
+//                inp20 = 8'h2; inp21 = 8'h3; inp22 = 8'h1; inp23 = 8'h0;
+//                inp30 = 8'h1; inp31 = 8'h2; inp32 = 8'h2; inp33 = 8'h1;
+//                inp40 = 8'h2; inp41 = 8'h0; inp42 = 8'h1; inp43 = 8'h0;
+//                ker10 = 8'h1; ker11 = 8'h0; ker12 = 8'h1;
+//                ker20 = 8'h0; ker21 = 8'h1; ker22 = 8'h0;
+//                ker30 = 8'h1; ker31 = 8'h1; ker32 = 8'h1;
+
+        // Wait for 10ns
+        #10;
+
+        // Display outputs
+        $display("Output out10: %h, out11: %h, out20: %h, out21: %h", out10, out11, out20, out21);
+
+        // End simulation
+        $finish;
+    end
+
+endmodule
